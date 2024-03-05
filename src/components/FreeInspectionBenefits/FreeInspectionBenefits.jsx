4548a851ce70ee95e1de457ff5766a65ed42777a
limematheuz <mribeiro17.info@gmail.com>
2024-03-05 20:35:23 +0100

    last component

--- src/components/FreeInspectionBenefits/FreeInspectionBenefits.jsx
+++ src/components/FreeInspectionBenefits/FreeInspectionBenefits.jsx
@@ -1,26 +1,35 @@
-import "./FreeInspectionBenefits.css"
+import "./FreeInspectionBenefits.css";
+
+import truck from "../../assets/icons/truck.svg";
+import worker from "../../assets/icons/worker.svg";
+import shield from "../../assets/icons/shield.svg";
+import money from "../../assets/icons/money.svg";
 
 export default function FreeInspectionBenefits() {
   return (
     <section className="free-inspection-intro-container">
-        <section className="cards-free-intro">
-          <div>
-            <img src="" alt="" />
-            <h3></h3>
+      <section className="cards-free-intro">
+        <section className="first-benefit-info">
+          <div className="first-card-info">
+            <img src={truck} alt="" />
+            <h3>fast attention</h3>
           </div>
-          <div>
-            <img src="" alt="" />
-            <h3></h3>
+          <div className="second-card-info">
+            <img src={worker} alt="" />
+            <h3>advice from a specialist</h3>
           </div>
-          <div>
-            <img src="" alt="" />
-            <h3></h3>
+        </section>
+        <section className="second-benefit-info">
+          <div className="second-card-info">
+            <img src={shield} alt="" />
+            <h3>certified experience</h3>
           </div>
-          <div>
-            <img src="" alt="" />
-            <h3></h3>
+          <div className="first-card-info">
+            <img src={money} alt="" />
+            <h3>INSURANCE CLAIM SPECIALISTS</h3>
           </div>
         </section>
       </section>
+    </section>
   );
 }
